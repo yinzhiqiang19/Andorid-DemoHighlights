a5bc8edd531de36300d3710d004b6a71f8f0b49f
yinzhiqiang19 <yinzhiqiang19@163.com>
2017-12-04 23:45:40 +0800

    import project
    导入android工程

---
+++ DemoHighlights/app/src/main/java/com/yinzhiqiang/demohighlights/sidemenu/model/SlideMenuItem.java
@@ -0,0 +1,29 @@
+package com.yinzhiqiang.demohighlights.sidemenu.model;
+
+import com.yinzhiqiang.demohighlights.sidemenu.interfaces.Resourceble;
+
+public class SlideMenuItem implements Resourceble {
+    private String name;
+    private int imageRes;
+
+    public SlideMenuItem(String name, int imageRes) {
+        this.name = name;
+        this.imageRes = imageRes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getImageRes() {
+        return imageRes;
+    }
+
+    public void setImageRes(int imageRes) {
+        this.imageRes = imageRes;
+    }
+}
